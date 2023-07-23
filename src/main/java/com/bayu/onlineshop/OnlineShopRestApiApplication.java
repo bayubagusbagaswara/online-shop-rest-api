b65632c0afd6a63ee8d3356e0c03dad69bb07943
Bayu Bagus Bagaswara <bayu.bagaswaraa@gmail.com>
2023-07-23 20:19:24 +0700

    Initial commit

---
+++ src/main/java/com/bayu/onlineshop/OnlineShopRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.bayu.onlineshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineShopRestApiApplication.class, args);
+	}
+
+}
